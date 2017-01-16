cbb2973c22903a0cff60d910f837a4d3c21e3c0f
jakub Cieciala <jakub.cieciala@astrumq.com>
2017-01-16 12:16:17 +0100

    Add cron hash

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -14,11 +14,14 @@ abstract class BasePresenter extends Presenter {
 	/** @var EntityManager @inject */
 	public $em;
 
+	/** @persistent */
+	public $cronHash;
+
 	public function startup() {
 		parent::startup();
 		/** Redirect unlogged users to homepage */
 		if (!$this->user->isLoggedIn()) {
-			if ($this->name !== 'Sign' && ($this->action !== 'in' || $this->action !== 'register')) {
+			if (($this->name !== 'Sign' && ($this->action !== 'in' || $this->action !== 'register')) || $this->cronHash !== 'cb7ddd82ced9a4e1afd7abcf13cd8b862475bf55cc8feba0bf95d1fc03bdc536') {
 				$this->redirect('Sign:in');
 			}
 		}
