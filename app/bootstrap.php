ce3ebdf614fa4cd505a13dfec00c3033eb9b6ffc
jakub Cieciala <jakub.cieciala@astrumq.com>
2017-02-18 09:02:56 +0100

    Update loader logic

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -9,7 +9,7 @@ function dd() {
 error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_USER_DEPRECATED & ~E_USER_WARNING & ~E_USER_NOTICE & ~E_WARNING & LOG_NOTICE);
 $configurator = new Nette\Configurator;
 
-$configurator->setDebugMode(TRUE); // enable for your remote IP
+//$configurator->setDebugMode(TRUE); // enable for your remote IP
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTimeZone('Europe/Prague');
